d09f4456601a0e2e9ba40c3c61dfb38e65e78521
kriszk <kkozsa@gmail.com>
2023-11-18 12:46:53 +0000

    Add project files.

---
+++ Slot machine simulator/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Slot_machine_simulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.amountTextbox = new System.Windows.Forms.TextBox();
+            this.spinButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(137, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter amount:";
+            // 
+            // amountTextbox
+            // 
+            this.amountTextbox.Location = new System.Drawing.Point(229, 81);
+            this.amountTextbox.Name = "amountTextbox";
+            this.amountTextbox.Size = new System.Drawing.Size(100, 20);
+            this.amountTextbox.TabIndex = 1;
+            // 
+            // spinButton
+            // 
+            this.spinButton.Location = new System.Drawing.Point(140, 333);
+            this.spinButton.Name = "spinButton";
+            this.spinButton.Size = new System.Drawing.Size(75, 23);
+            this.spinButton.TabIndex = 2;
+            this.spinButton.Text = "Spin";
+            this.spinButton.UseVisualStyleBackColor = true;
+            this.spinButton.Click += new System.EventHandler(this.spinButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(562, 333);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(100, 167);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(246, 167);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(390, 167);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.spinButton);
+            this.Controls.Add(this.amountTextbox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Slot machine simulator";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox amountTextbox;
+        private System.Windows.Forms.Button spinButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Slot machine simulator/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Slot_machine_simulator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int inputMoney = 0;     
+        int won = 0;
+        Random random = new Random();       // Random class to generate random number
+        string[] fruitImages = { "apple.bmp", "banana.bmp", "cherries.bmp", "grapes.bmp", "lemon.bmp", "lime.bmp" };
+        // String array to store files of fruit images.
+
+        private void spinButton_Click(object sender, EventArgs e)
+        {
+            int insertMoney;
+            if (int.TryParse(amountTextbox.Text, out insertMoney))
+            {
+                if (insertMoney <= 0)       // Check if the amount is above 0
+                {
+                    MessageBox.Show("Please insert a valid amount.");
+                    return;
+                }
+
+                inputMoney += insertMoney;
+
+                string fruit1 = Path.Combine("C:\\Users\\KK\\Desktop\\DBS\\Object oriented programming\\CA\\Slot machine simulator\\Images", fruitImages[random.Next(fruitImages.Length)]);
+                string fruit2 = Path.Combine("C:\\Users\\KK\\Desktop\\DBS\\Object oriented programming\\CA\\Slot machine simulator\\Images", fruitImages[random.Next(fruitImages.Length)]);
+                string fruit3 = Path.Combine("C:\\Users\\KK\\Desktop\\DBS\\Object oriented programming\\CA\\Slot machine simulator\\Images", fruitImages[random.Next(fruitImages.Length)]);
+                // Strings   generate a random number between 0 and the length of the fruitImages array.   
+
+                pictureBox1.Image = Image.FromFile(fruit1);     // Adds images to the pictureboxes.
+                pictureBox2.Image = Image.FromFile(fruit2);
+                pictureBox3.Image = Image.FromFile(fruit3);
+
+                if (fruit1 == fruit2 && fruit2 == fruit3)
+                {
+                    int match = 3;
+                    int winning = inputMoney * match;
+                    won += winning;
+                    MessageBox.Show("Congratulations, you have won " + winning);
+
+                }
+
+                else if (fruit1 == fruit2 || fruit1 == fruit3 || fruit2 == fruit3)
+                {
+                    int match = 2;
+                    int winning = inputMoney * match;
+                    won += winning;
+                    MessageBox.Show("Congratulations, you have won " + winning);
+
+                }
+
+                else
+                {
+                    MessageBox.Show("Sorry, you did not win.");
+                }
+
+            }
+            else
+            {
+                MessageBox.Show(" Enter a valid amount.");
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Total amount played: " + inputMoney );
+            MessageBox.Show("Total amount won: " + won);
+            this.Close();
+        }
+
+        
+    }
+}
